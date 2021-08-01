ed602ff584ce2ff01c1585dd5b605e97f9f67854
Benjamin Barbe <benjamin@pandasuite.com>
2021-08-01 14:48:41 +0200

    Atrament component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import PandaBridge from 'pandasuite-bridge';
+import Atrament from 'atrament';
+
 import './index.css';
 
 let properties = null;
-let markers = null;
+
+let sketchpad = null;
+const strokes = [];
 
 function myInit() {
-  // const imageUrl = PandaBridge.resolvePath('my_image.png');
-  // PandaBridge.send('imageChanged');
+  const canvas = document.querySelector('#sketchpad');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  sketchpad = new Atrament(canvas);
+  sketchpad.recordStrokes = true;
+
+  sketchpad.adaptiveStroke = properties.adaptiveStroke;
+  sketchpad.smoothing = properties.smoothing;
+
+  sketchpad.addEventListener('dirty', () => PandaBridge.send('dirty'));
+  sketchpad.addEventListener('clean', () => PandaBridge.send('clean'));
+
+  sketchpad.addEventListener('strokerecorded', (obj) => {
+    if (!sketchpad.recordPaused) {
+      strokes.push(obj.stroke);
+    }
+  });
 }
 
 PandaBridge.init(() => {
   PandaBridge.onLoad((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
 
     if (document.readyState === 'complete') {
       myInit();
@@ -23,22 +42,68 @@ PandaBridge.init(() => {
 
   PandaBridge.onUpdate((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
-  });
-
-  /* Markers */
-
-  PandaBridge.getSnapshotData(() => null);
-
-  PandaBridge.setSnapshotData((pandaData) => {
-    // pandaData.data.id
   });
 
   /* Actions */
 
-  PandaBridge.listen('changeColor', (args) => {
+  PandaBridge.listen('changeColor', ([params]) => {
+    const { color } = params || {};
+
+    sketchpad.color = color;
   });
 
-  PandaBridge.synchronize('synchroImages', (percent) => {
+  PandaBridge.listen('changeWeight', ([params]) => {
+    const { weight } = params || {};
+
+    sketchpad.weight = weight;
+  });
+
+  PandaBridge.listen('changeMode', ([params]) => {
+    const { mode } = params || {};
+
+    sketchpad.mode = mode;
+  });
+
+  PandaBridge.listen('clear', () => {
+    sketchpad.clear();
+  });
+
+  PandaBridge.listen('undo', () => {
+    strokes.pop();
+    sketchpad.clear();
+    sketchpad.recordPaused = true;
+    strokes.forEach((stroke) => {
+      // set drawing options
+      sketchpad.mode = stroke.mode;
+      sketchpad.weight = stroke.weight;
+      sketchpad.smoothing = stroke.smoothing;
+      sketchpad.color = stroke.color;
+      sketchpad.adaptiveStroke = stroke.adaptiveStroke;
+
+      // don't want to modify original data
+      const points = stroke.points.slice();
+
+      const firstPoint = points.shift();
+      // beginStroke moves the "pen" to the given position and starts the path
+      sketchpad.beginStroke(firstPoint.x, firstPoint.y);
+
+      let prevPoint = firstPoint;
+      while (points.length > 0) {
+        const point = points.shift();
+
+        // the `draw` method accepts the current real coordinates
+        // (i. e. actual cursor position), and the previous processed (filtered)
+        // position. It returns an object with the current processed position.
+        const { x, y } = sketchpad.draw(point.x, point.y, prevPoint.x, prevPoint.y);
+
+        // the processed position is the one where the line is actually drawn to
+        // so we have to store it and pass it to `draw` in the next step
+        prevPoint = { x, y };
+      }
+
+      // endStroke closes the path
+      sketchpad.endStroke(prevPoint.x, prevPoint.y);
+    });
+    sketchpad.recordPaused = false;
   });
 });
